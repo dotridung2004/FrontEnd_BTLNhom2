0450f8734aa576ae432282f53aa90ec7c0d5e98e
dsharko1505 <145703949+dsharko1505@users.noreply.github.com>
2025-10-22 10:58:31 +0700

    update

--- lib/screens/leave_makeup_screen.dart
+++ lib/screens/leave_makeup_screen.dart
@@ -703,7 +703,7 @@ Future<bool?> showSuccessDialog(BuildContext context, String message) {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             const Text(
-              'Thông báo!',
+              ' báo!',
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
             IconButton(
